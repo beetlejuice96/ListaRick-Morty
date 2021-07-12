925cee4d65d7a620f72d72567bb249a4fae39382
beetlejuice96 <dilanbernbae043@gmail.com>
2021-07-11 22:07:01 -0300

    agrego configuracion de webpack

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ tambien tiene un buscador que actua sobre la lista.
 este repo esta hecho meramente para practicar React
 
 Link:https://naughty-jennings-417c6e.netlify.app
+
+
+## Este proyecto tambien se utilizo para poder practicar webpack.
--- src/App.js
+++ src/App.js
@@ -1,113 +1,118 @@
-import logo from './logo.svg';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './App.css';
-import Table from './components/Table.js';
-import escapeStringRegexp from 'escape-string-regexp';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+import Table from "./components/Table.js";
+import escapeStringRegexp from "escape-string-regexp";
 
 class App extends React.Component {
-
   constructor(props) {
     super(props);
-    this.state={
-      data:[],
-      dataView:[]
-    }
+    this.state = {
+      data: [],
+      dataView: [],
+    };
   }
 
-  obtenerPersonajes = () =>{
+  obtenerPersonajes = () => {
     //TODO hacer array con nros random y pasarlo a la url.
-    let url ='https://rickandmortyapi.com/api/character/1,2,3,4,5,6,7,8,9,10';
+    let url = "https://rickandmortyapi.com/api/character/1,2,3,4,5,6,7,8,9,10";
     fetch(url)
-        .then(res => res.json())
-        .then((personajeCard) => {
-          this.setState({
-            data:personajeCard,
-            dataView:personajeCard,
-          });
-        })
-        .catch(console.log)
-  }
+      .then((res) => res.json())
+      .then((personajeCard) => {
+        this.setState({
+          data: personajeCard,
+          dataView: personajeCard,
+        });
+      })
+      .catch(console.log);
+  };
 
-  handleChange=(e)=> {
-      let aux = [];
-      this.state.data.map((row) => {
-          /*if (row.name.toLowerCase().includes(e.target.value.toLowerCase())) {
+  handleChange = (e) => {
+    let aux = [];
+    this.state.data.map((row) => {
+      /*if (row.name.toLowerCase().includes(e.target.value.toLowerCase())) {
               aux.push(row)
           }*/
-          /*TODO ver como poder recorrer verdaderamente todos los campos anidados de un objeto.*/
-          const regex = new RegExp(`.*?${escapeStringRegexp(e.target.value)}.*?`, 'i');
-          const rowKeys=['id','name','species'];
+      /*TODO ver como poder recorrer verdaderamente todos los campos anidados de un objeto.*/
+      const regex = new RegExp(
+        `.*?${escapeStringRegexp(e.target.value)}.*?`,
+        "i"
+      );
+      const rowKeys = ["id", "name", "species"];
 
-          for (let i = 0; i<rowKeys.length;i++){
-              let value = row[rowKeys[i]];
-              if(regex.test(value)){
-                  aux.push(row);
-                  break;
-              }
-          }
-      })
-      if (e.target.value === ''){
-          this.setState((prev)=>({
-              ...prev,
-              dataView:this.state.data,
-          }))
-      }else {
-          this.setState((prev)=>({
-              ...prev,
-              dataView:aux,
-          }))
+      for (let i = 0; i < rowKeys.length; i++) {
+        let value = row[rowKeys[i]];
+        if (regex.test(value)) {
+          aux.push(row);
+          break;
+        }
       }
+    });
+    if (e.target.value === "") {
+      this.setState((prev) => ({
+        ...prev,
+        dataView: this.state.data,
+      }));
+    } else {
+      this.setState((prev) => ({
+        ...prev,
+        dataView: aux,
+      }));
+    }
+  };
 
-  }
-
-  deleteRow =(id)=>{
-      const newArray = this.state.data.filter(row => row.id !== id);
-      this.setState({
-          data:newArray,
-          dataView:newArray
-      })
-  }
-
-  editRow=(row)=>{
-    const {id,name,species}=row;
-    const newArray = this.state.data.map(personaje=>personaje.id===row.id ? {...personaje ,id:id,name:name,species:species} : personaje);
+  deleteRow = (id) => {
+    const newArray = this.state.data.filter((row) => row.id !== id);
     this.setState({
-        data:newArray,
-        dataView:newArray
-    })
-  }
+      data: newArray,
+      dataView: newArray,
+    });
+  };
 
-  render(){
-      const style= {
-          background:'#282c34',
-          color:'white',
-      }
-      const styleh1 = {
-          margin:'auto',
-      }
-      const stylenav = {
-          display:'flex',
-          flexDirection:'column',
+  editRow = (row) => {
+    const { id, name, species } = row;
+    const newArray = this.state.data.map((personaje) =>
+      personaje.id === row.id
+        ? { ...personaje, id: id, name: name, species: species }
+        : personaje
+    );
+    this.setState({
+      data: newArray,
+      dataView: newArray,
+    });
+  };
 
-      }
-    return(
-
-        <React.Fragment>
-            <header className="navbar navbar-expand-lg " style={style}>
-                <h1 style={styleh1}>Lista de rick & Morty</h1>
-            </header>
-            <nav className="navbar navbar-expand-lg ">
-                <button className="btn btn-info" onClick={this.obtenerPersonajes}>Listar personajes</button>
-                <input type="text" className="ms-4" onChange={this.handleChange}/>
-            </nav>
-            <Table dataView ={this.state.dataView} deleteRow={this.deleteRow} editRow={this.editRow}/>
-
-        </React.Fragment>
-    )
+  render() {
+    const style = {
+      background: "#282c34",
+      color: "white",
+    };
+    const styleh1 = {
+      margin: "auto",
+    };
+    const stylenav = {
+      display: "flex",
+      flexDirection: "column",
+    };
+    return (
+      <React.Fragment>
+        <header className="navbar navbar-expand-lg " style={style}>
+          <h1 style={styleh1}>Lista de rick & Morty</h1>
+        </header>
+        <nav className="navbar navbar-expand-lg ">
+          <button className="btn btn-info" onClick={this.obtenerPersonajes}>
+            Listar personajes
+          </button>
+          <input type="text" className="ms-4" onChange={this.handleChange} />
+        </nav>
+        <Table
+          dataView={this.state.dataView}
+          deleteRow={this.deleteRow}
+          editRow={this.editRow}
+        />
+      </React.Fragment>
+    );
   }
 }
 
 export default App;
-
